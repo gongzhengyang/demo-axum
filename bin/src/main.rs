d2256c4f1f40899df51ac75ed247b494b81781e4
gongzhengyang <gongzhengyang@bolean.com.cn>
2022-12-02 18:33:05 +0800

    feat: 增加api和entity部分

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,6 +1,6 @@
 use db::get_db_connection;
 pub use migration::{Migrator, MigratorTrait};
-
+use axum::{Router};
 use tokio;
 
 #[tokio::main]
@@ -11,5 +11,17 @@ async fn main() -> anyhow::Result<()>{
     let db = get_db_connection().await;
     Migrator::up(db, None).await?;
 
+
+    // let server_url
     Ok(())
 }
+
+fn get_server_url() -> String {
+    let host = std::env::var("SERVER_HOST", "0.0.0.0");
+    let port = env!("SERVER_PORT", "8088");
+    format!("{}:{}", host, port)
+}
+
+// fn get_app_router() {
+//     Router::new()
+// }
